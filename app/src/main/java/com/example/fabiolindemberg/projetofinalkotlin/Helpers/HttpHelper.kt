b264ce152cd72e7841637ad26dd26887bd5fca4c
Fábio Lindemberg <fabiolindemberg>
2019-02-20 16:56:19 -0300

    HTTPHelper Adding static class HTTPHelper to make easy do http requests

---
+++ app/src/main/java/com/example/fabiolindemberg/projetofinalkotlin/Helpers/HttpHelper.kt
@@ -0,0 +1,46 @@
+package com.example.fabiolindemberg.projetofinalkotlin.Helpers
+
+import okhttp3.*
+import java.io.IOException
+
+object HttpHelper {
+    private const val TAG = "http"
+    private const val LOG_ON = true
+    private val JSON = MediaType.parse("application/json; charset=utf-8")
+    var client = OkHttpClient()
+
+    fun get(url: String):String {
+        val request = Request.Builder().url(url).get().build()
+        return getJson(request)
+    }
+
+    fun post(url: String, json: String):String{
+        val request = Request.Builder().url(url).post(RequestBody.create(JSON, json)).build()
+        return getJson(request)
+    }
+
+    fun postFrom(url: String, params: Map<String, String>):String{
+        var builder = FormBody.Builder()
+
+        for ((key, value) in params){
+            builder.add(key, value)
+        }
+
+        var body = builder.build()
+
+        val request = Request.Builder().url(url).post(body).build()
+
+        return getJson(request)
+    }
+
+    private fun getJson(request: Request): String {
+        val response = client.newCall(request).execute()
+        val responseBody = response.body()
+        if(responseBody != null){
+            val json = responseBody.string()
+            return json
+        }
+        throw IOException("Erro ao fazer requisição")
+    }
+
+}
